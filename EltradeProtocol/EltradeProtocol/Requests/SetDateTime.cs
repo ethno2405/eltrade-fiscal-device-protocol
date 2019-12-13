87ed2097679128894baa1aa00e662bff3288fbbd
Konstantin Tenekedzhiev <konstantin.tenekedzhiev@eldersoss.com>
2019-12-13 17:09:20 +0200

    Oops

--- EltradeProtocol/EltradeProtocol/Requests/SetDateTime.cs
+++ EltradeProtocol/EltradeProtocol/Requests/SetDateTime.cs
@@ -5,6 +5,6 @@ namespace EltradeProtocol.Requests
     public class SetDateTime : EltradeFiscalDeviceRequestPackage
     {
         public SetDateTime() : this(DateTime.Now) { }
-        public SetDateTime(DateTime dateTime) : base(0x3d, DateTime.Now.ToString("dd-MM-y HH:mm:ss")) { }
+        public SetDateTime(DateTime dateTime) : base(0x3d, dateTime.ToString("dd-MM-y HH:mm:ss")) { }
     }
 }
