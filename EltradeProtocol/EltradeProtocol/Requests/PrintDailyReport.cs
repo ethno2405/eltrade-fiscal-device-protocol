d526c83eb50bac464f8dffb8abff78a2b5128cbd
Konstantin Tenekedzhiev <konstantin.tenekedzhiev@eldersoss.com>
2019-12-16 18:27:29 +0200

    Fixed typo

---
+++ EltradeProtocol/EltradeProtocol/Requests/PrintDailyReport.cs
@@ -0,0 +1,8 @@
+﻿namespace EltradeProtocol.Requests
+{
+    public class PrintDailyReport : EltradeFiscalDeviceRequestPackage
+    {
+        public PrintDailyReport() : this(false) { }
+        public PrintDailyReport(bool zero) : base(0x45, zero ? "0" : "2") { }
+    }
+}
--- EltradeProtocol/EltradeProtocol/Requests/PrintDaylyReport.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace EltradeProtocol.Requests
-{
-    public class PrintDaylyReport : EltradeFiscalDeviceRequestPackage
-    {
-        public PrintDaylyReport() : this(false) { }
-        public PrintDaylyReport(bool zero) : base(0x45, zero ? "0" : "2") { }
-    }
-}
