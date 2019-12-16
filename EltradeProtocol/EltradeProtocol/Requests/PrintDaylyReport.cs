e97b838642b548b7a6f95657755fbcb04dfcef0d
Konstantin Tenekedzhiev <konstantin.tenekedzhiev@eldersoss.com>
2019-12-16 18:19:07 +0200

    Print daily reports

---
+++ EltradeProtocol/EltradeProtocol/Requests/PrintDaylyReport.cs
@@ -0,0 +1,8 @@
+﻿namespace EltradeProtocol.Requests
+{
+    public class PrintDaylyReport : EltradeFiscalDeviceRequestPackage
+    {
+        public PrintDaylyReport() : this(false) { }
+        public PrintDaylyReport(bool zero) : base(0x45, zero ? "0" : "2") { }
+    }
+}
