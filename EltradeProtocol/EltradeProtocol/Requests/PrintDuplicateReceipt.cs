95a0618676832c87e2c1d41e1046ef7d56abc2c5
Konstantin Tenekedzhiev <konstantin.tenekedzhiev@eldersoss.com>
2019-12-16 18:10:34 +0200

    Print duplicate receipt

---
+++ EltradeProtocol/EltradeProtocol/Requests/PrintDuplicateReceipt.cs
@@ -0,0 +1,7 @@
+﻿namespace EltradeProtocol.Requests
+{
+    public class PrintDuplicateReceipt : EltradeFiscalDeviceRequestPackage
+    {
+        public PrintDuplicateReceipt() : base(0x6d) { }
+    }
+}
