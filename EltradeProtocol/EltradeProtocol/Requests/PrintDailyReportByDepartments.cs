1d83398004fee0c58d88659bf257fbdf1d939060
Konstantin Tenekedzhiev <konstantin.tenekedzhiev@eldersoss.com>
2019-12-16 18:26:56 +0200

    Print daily reports by department and PLU

---
+++ EltradeProtocol/EltradeProtocol/Requests/PrintDailyReportByDepartments.cs
@@ -0,0 +1,8 @@
+﻿namespace EltradeProtocol.Requests
+{
+    public class PrintDailyReportByDepartments : EltradeFiscalDeviceRequestPackage
+    {
+        public PrintDailyReportByDepartments() : this(false) { }
+        public PrintDailyReportByDepartments(bool zero) : base(0x75, zero ? "0" : "2") { }
+    }
+}
---
+++ EltradeProtocol/EltradeProtocol/Requests/PrintDailyReportByDepartmentsAndArticles.cs
@@ -0,0 +1,8 @@
+﻿namespace EltradeProtocol.Requests
+{
+    public class PrintDailyReportByDepartmentsAndArticles : EltradeFiscalDeviceRequestPackage
+    {
+        public PrintDailyReportByDepartmentsAndArticles() : this(false) { }
+        public PrintDailyReportByDepartmentsAndArticles(bool zero) : base(0x76, zero ? "0" : "2") { }
+    }
+}
