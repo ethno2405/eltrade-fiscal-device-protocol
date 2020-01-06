1b560994bd370ab72dc41a47cb2791ad72dcabab
Konstantin Tenekedzhiev <konstantin.tenekedzhiev@eldersoss.com>
2020-01-06 11:18:40 +0200

    Get transaction status

---
+++ EltradeProtocol/EltradeProtocol/Requests/GetTransactionStatus.cs
@@ -0,0 +1,7 @@
+﻿namespace EltradeProtocol.Requests
+{
+    public class GetTransactionStatus : EltradeFiscalDeviceRequestPackage
+    {
+        public GetTransactionStatus() : base(0x4c, "T") { }
+    }
+}
