7bb9074b3294343d7d4e0d81c9416e3318b4937d
Konstantin Tenekedzhiev <konstantin.tenekedzhiev@eldersoss.com>
2020-01-10 12:26:07 +0200

    Print monthly and yearly fiscal reports

---
+++ EltradeProtocol/EltradeProtocol/Requests/PrintMonthlyFiscalReport.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace EltradeProtocol.Requests
+{
+    public class PrintMonthlyFiscalReport : EltradeFiscalDeviceRequestPackage
+    {
+        public PrintMonthlyFiscalReport(DateTime start) : base(0x4f, $"{start.ToString("MMy")}") { }
+    }
+}
---
+++ EltradeProtocol/EltradeProtocol/Requests/PrintYearlyFiscalReport.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace EltradeProtocol.Requests
+{
+    public class PrintYearlyFiscalReport : EltradeFiscalDeviceRequestPackage
+    {
+        public PrintYearlyFiscalReport(DateTime start) : base(0x4f, $"{start.ToString("yy")}") { }
+    }
+}
